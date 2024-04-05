0985e886b56e957f9e03ca17eefbcabbf55d5166
Rushikesh Powar <114715746+Rushipowar@users.noreply.github.com>
2024-04-04 20:16:30 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import * as XLSX from "xlsx";
+import logo from "./assets/addaff-logo.png"
+
+import './App.css';
+
+function App() {
+
+  const [data, setData] = useState([]);
+
+  const handleFileUpload = (e) => {
+    const reader = new FileReader();
+    reader.readAsBinaryString(e.target.files[0]);
+    reader.onload = (e) => {
+      const data = e.target.result;
+      const workbook = XLSX.read(data, { type: "binary" });
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const parsedData = XLSX.utils.sheet_to_json(sheet);
+      setData(parsedData);
+    };
+  }
+
+  return (
+    <div className="App">
+
+      <div className="Choose-container">
+      <input 
+        className="Choose-button"
+        type="file" 
+        accept=".xlsx, .xls" 
+        onChange={handleFileUpload} 
+      />
+      </div>
+
+      {data.length > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              {Object.keys(data[0]).map((key) => (
+                <th key={key}>{key}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                {Object.values(row).map((value, index) => (
+                  <td key={index}>{value}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+
+      <br /><br />
+     <div className="logo-container">
+      <img src={logo} className="Logo-image"></img>
+     </div>
+    </div>
+  );
+}
+
+export default App;
